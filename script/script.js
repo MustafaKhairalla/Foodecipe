517276d8d1e4ce020765ee6d493f6365d889c38e
cristian634 <cristian634@comcast.net>
2020-01-28 21:16:03 -0700

    js pulls drop down pulls api call

---
+++ script/script.js
@@ -0,0 +1,61 @@
+selectorEl = $("#selector");
+
+
+$(document).ready(function () {
+    selectorEl.on("change", function(){
+        
+        var q = $(this).val();
+        console.log(q);
+        var settings = {
+        
+            "async": true,
+            "crossDomain": true,
+            "url": "https://tasty.p.rapidapi.com/recipes/list?tags=under_30_minutes&from=0&sizes=20&q="+q,
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "tasty.p.rapidapi.com",
+                "x-rapidapi-key": "cc8f6d051amsh50057cf210b930cp1ef7a5jsnce9dccdb92a1"
+            }
+        }
+    
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+    
+    
+    
+            // var recipeTile = response.results[0].name;
+            // var displayRecipe = $("#itemDisplay").prepend("<h4>" + recipeTile);
+    
+            // console.log(recipeTile);
+            // //where ingredients are stored 
+            // var componentsList = response.results[0].sections[0].components;
+            // var ingredientList = $("#ingredientList");
+            // var newItem = $("<ls>");
+            // //raw string of ingredients
+            // var newItemText = componentsList.raw_text;
+            // console.log(response.results[0].sections[0].components)
+    
+            // for (var i = 0; i < componentsList.length; i++) {
+            //     //parse the list of items
+            //     newItemText = componentsList[i].raw_text;
+            //     console.log(newItemText);
+            //     //add new html element 
+            //     ingredientList.append(newItem).append("<li>" + newItemText);
+            // };
+    
+            // var instructions = response.results[0].instructions[0].display_text;
+            // console.log(instructions);
+            // var instructionList = $("#instructionList");
+    
+            // for (i = 0; instructions.length; i++) {
+            //     instuction = response.results[i].instructions[i].display_text;
+            //     console.log(instructions);
+            //     instructionList.append(newItem).append("<li>" + instructions);
+            // };
+        });
+    });
+
+    
+    
+
+})
