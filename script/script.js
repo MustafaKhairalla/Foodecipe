9387d1bbac472a2b57d4fff309faa8d79d59eeed
cristian634 <cristian634@comcast.net>
2020-02-01 13:01:42 -0700

    cards displaying array info

--- script/script.js
+++ script/script.js
@@ -1,106 +1,12 @@
 selectorEl = $("#selector");
 // let totalArray = // some call for default view || [];
-var rowClass = document.getElementById("uk-child-width-1-3@m uk-grid-small uk-grid-match");
+var rowClass = document.getElementById("cardsDisplay");
 
-    $(document).ready(function () {
-        selectorEl.on("change", function () {
 
-<<<<<<< HEAD
-            var q = $(this).val();
-            console.log(q);
-            var settings = {
 
-                "async": true,
-                "crossDomain": true,
-                "url": "https://tasty.p.rapidapi.com/recipes/list?&from=0&size=6&q=" + q,
-                "method": "GET",
-                "headers": {
-                    "x-rapidapi-host": "tasty.p.rapidapi.com",
-                    "x-rapidapi-key": "cc8f6d051amsh50057cf210b930cp1ef7a5jsnce9dccdb92a1"
-                }
-            }
 
-            $.ajax(settings).done(function (response) {
-                console.log(response);
 
-                function findDescription() {
-
-                }
-                for (i = 0; i < response.results.length; i++) {
-                    console.log("for loop hit")
-                    var recipeTitle = response.results[i].name;
-                    var newDescription = response.results[i].description;
-                    var newImage = response.results[i].thumbnail_url;
-                    console.log(recipeTitle);
-                    console.log(newDescription);
-                    console.log($(".uk-card-title"));
-                    // var mainSection = document.getElementById("main");
-                    //var rowClass = document.querySelector("uk-child-width-1-3@m uk-grid-small uk-grid-match");
-                    // add it to a row 
-                    // var rowClass = document.createElement("div");
-                    // rowClass.setAttribute("class", "uk-child-width-1-3@m uk-grid-small uk-grid-match");
-                    // Create Cards
-                    var newCard = document.createElement("div");
-                    newCard.setAttribute("class", "uk-card uk-card-default uk-border-rounded");
-                        var div2 = document.createElement("div"); 
-                        //div2.setAttribute("class","uk-card uk-card-default uk-card-mediuk-card-media-top uk-cover-container uk-inline-clip uk-transition-togglea-top uk-cover-container"); 
-                            var imgThumb = document.createElement("img");
-                            imgThumb.setAttribute("class", "uk-transition-scale-up uk-transition-opaque");
-                            imgThumb.setAttribute("src", newImage);
-                            console.log(imgThumb);
-                            var div3 = document.createElement("div");
-                            div3.setAttribute("class", "uk-padding-small");
-                                var titleSet = document.createElement("h3");
-                                titleSet.setAttribute("class","uk-card-title", recipeTitle);
-                                titleSet = recipeTitle;
-                                var descriptSet = document.createElement("p");
-                                descriptSet = newDescription;
-                    
-                        // <ul>
-                            // for loop to generate n <li> nodes
-                            
-                    //rowClass.append(div2,titleSet,descriptSet);
-
-                    div3.append(titleSet, descriptSet);
-                    div2.append(imgThumb)
-                    //div2.append(imgThumb, div3);
-                    newCard.append(div2, div3);
-                    rowClass.append(newCard);
-                    // mainSection.append(rowClass);
-                    console.log(rowClass);
-                };
-                // var recipeTile = response.results[0].name;
-                // var displayRecipe = $("#itemDisplay").prepend("<h4>" + recipeTile);
-
-                // console.log(recipeTile);
-                // //where ingredients are stored 
-                // var componentsList = response.results[0].sections[0].components;
-                // var ingredientList = $("#ingredientList");
-                // var newItem = $("<ls>");
-                // //raw string of ingredients
-                // var newItemText = componentsList.raw_text;
-                // console.log(response.results[0].sections[0].components)
-
-                // for (var i = 0; i < componentsList.length; i++) {
-                //     //parse the list of items
-                //     newItemText = componentsList[i].raw_text;
-                //     console.log(newItemText);
-                //     //add new html element 
-                //     ingredientList.append(newItem).append("<li>" + newItemText);
-                // };
-
-                // var instructions = response.results[0].instructions[0].display_text;
-                // console.log(instructions);
-                // var instructionList = $("#instructionList");
-
-                // for (i = 0; instructions.length; i++) {
-                //     instuction = response.results[i].instructions[i].display_text;
-                //     console.log(instructions);
-                //     instructionList.append(newItem).append("<li>" + instructions);
-                // };
-            });
-        });
-=======
+// -------------------------------- AJAX CALL FOR API -------------------------------
 $(document).ready(function () {
     selectorEl.on("change", function () {
 
@@ -123,34 +29,58 @@ $(document).ready(function () {
 
             console.log(response);
 
-
+           
             for (i = 0; i < response.results.length; i++) {
                 recipeArray = [];
                 responseId = response.results[i].canonical_id;
 
                 for (k = 0; k < 6; k++) {
                     if (responseId.includes("recipe")) {
-                        recipeArray.push(response.results[i]);
+                        recipeArray.push(response.results[k]);
+
 
                     }
                 }
 
-
-
-
-
             };
 
             for (l = 0; l < recipeArray.length; l++) {
-                var recipeTile = recipeArray[l].name;
+                var recipeTitle = recipeArray[l].name;
                 var sections = recipeArray[l].sections
 
                 var componentsList = sections && sections[0].components;
                 var instructionList = recipeArray[l].instructions;
-
+                var newThumb = recipeArray[l].thumbnail_url;
                 var newDescription = recipeArray[l].description;
-                console.log("Title: " + recipeTile);
+                console.log("Title: " + recipeTitle);
                 console.log("Description: " + newDescription);
+                console.log("Thumbnail: " + newThumb);
+
+                // ------------------ create elements with array info --------------------------
+
+
+                var newCard = document.createElement("div");
+                newCard.setAttribute("class", "uk-card uk-card-default uk-border-rounded");
+                var div2 = document.createElement("div");
+                var imgThumb = document.createElement("img");
+                imgThumb.setAttribute("class", "uk-transition-scale-up uk-transition-opaque");
+                imgThumb.setAttribute("src", newThumb);
+                // console.log(imgThumb);
+                var div3 = document.createElement("div");
+                div3.setAttribute("class", "uk-padding-small");
+                var titleSet = document.createElement("h3");
+                titleSet.setAttribute("class", "uk-card-title", recipeTitle);
+                titleSet = recipeTitle;
+                var descriptSet = document.createElement("p");
+                descriptSet = newDescription;
+
+
+
+                div3.append(titleSet, descriptSet);
+                div2.append(imgThumb)
+                newCard.append(div2, div3);
+                rowClass.append(newCard);
+
 
                 if (componentsList) {
                     for (var j = 0; j < componentsList.length; j++) {
@@ -159,73 +89,83 @@ $(document).ready(function () {
                     };
                 }
 
-                if(instructionList){
-                    for (var m = 0; m < instructionList.length; m++ ){
+                if (instructionList) {
+                    for (var m = 0; m < instructionList.length; m++) {
                         newInstruction = instructionList[m].display_text;
                         console.log(newInstruction);
                     };
                 };
-                console.log("--------------new recipe----------------")
+                console.log("--------------new recipe----------------");
             };
 
+
+
+            // for (i = 0; i < recipeArray.length; i++) {
+
+                // console.log(recipeTitle);
+                // console.log(newDescription);
+                // console.log($(".uk-card-title"));
+
+                // var newCard = document.createElement("div");
+                // newCard.setAttribute("class", "uk-card uk-card-default uk-border-rounded");
+                // var div2 = document.createElement("div");
+                // var imgThumb = document.createElement("img");
+                // imgThumb.setAttribute("class", "uk-transition-scale-up uk-transition-opaque");
+                // imgThumb.setAttribute("src", newImage);
+                // // console.log(imgThumb);
+                // var div3 = document.createElement("div");
+                // div3.setAttribute("class", "uk-padding-small");
+                // var titleSet = document.createElement("h3");
+                // titleSet.setAttribute("class", "uk-card-title", recipeTitle);
+                // titleSet = recipeTitle;
+                // var descriptSet = document.createElement("p");
+                // descriptSet = newDescription;
+
+
+
+                // div3.append(titleSet, descriptSet);
+                // div2.append(imgThumb)
+                // newCard.append(div2, div3);
+                // rowClass.append(newCard);
+                // console.log(rowClass);
+
+
+            // };
+            console.log(recipeArray.length)
             console.log(recipeArray);
-            // var recipeTile = response.results[0].name;
-            // var displayRecipe = $("#itemDisplay").prepend("<h4>" + recipeTile);
 
-            // console.log(recipeTile);
-            // //where ingredients are stored 
 
-            // var ingredientList = $("#ingredientList");
-            // var newItem = $("<ls>");
-            // //raw string of ingredients
-            // var newItemText = componentsList.raw_text;
-            // console.log(response.results[0].sections[0].components)
-
-            // for (var i = 0; i < componentsList.length; i++) {
-            //     //parse the list of items
-            //     newItemText = componentsList[i].raw_text;
-            //     console.log(newItemText);
-            //     //add new html element 
-            //     // ingredientList.append(newItem).append("<li>" + newItemText);
-            // };
-
-            // var instructions = response.results[0].instructions[0].display_text;
-            // console.log(instructions);
-            // var instructionList = $("#instructionList");
-
-            // for (i = 0; instructions.length; i++) {
-            //     instuction = response.results[i].instructions[i].display_text;
-            //     console.log(instructions);
-            //     instructionList.append(newItem).append("<li>" + instructions);
-            // };
         });
-    });
-    var settingsQuote = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://andruxnet-random-famous-quotes.p.rapidapi.com/?count=1",
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "andruxnet-random-famous-quotes.p.rapidapi.com",
-            "x-rapidapi-key": "07a1619e66msh10bf90c8c184833p1c1590jsn24047b7c532a"
+
+
+        //---------------------------QUOTE OF THE DAY---------------------------
+        var settingsQuote = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://andruxnet-random-famous-quotes.p.rapidapi.com/?count=1",
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "andruxnet-random-famous-quotes.p.rapidapi.com",
+                "x-rapidapi-key": "07a1619e66msh10bf90c8c184833p1c1590jsn24047b7c532a"
+            }
         }
-    }
-    
-    $.ajax(settingsQuote).done(function (response) {
-        console.log(response);
-        $("#quote").text('"' + response[0].quote + '" - ' + response[0].author);
-    });
+
+        $.ajax(settingsQuote).done(function (response) {
+            console.log(response);
+            $("#quote").text('"' + response[0].quote + '" - ' + response[0].author);
+        });
 
 
->>>>>>> origin
 
 
 
 
     })
+    //---------------------------CARD CLICK------------------------------
+    $(".uk-card").on("click", function () {
+        var cardID = $(this).text();
+        console.log("register card click" + cardID);
+        callAPI(cardID);
+    })
 
-$(".uk-card").on("click", function () {
-    var cardID = $(this).text();
-    console.log("register card click" + cardID);
-    callAPI(cardID);
-})
+});
