646d0f443d6a0e1c144cd9c7b8f10747fb31ac38
cristian634 <cristian634@comcast.net>
2020-02-01 11:28:05 -0700

    pull elements from array, ready to be displayed

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ selectorEl = $("#selector");
 
 $(document).ready(function () {
     selectorEl.on("change", function () {
-
+        //search paramater pull from selector bar 
         var q = $(this).val();
         console.log(q);
         var settings = {
@@ -25,10 +25,13 @@ $(document).ready(function () {
 
 
             for (i = 0; i < response.results.length; i++) {
+                //blank array for new recipes
                 recipeArray = [];
                 responseId = response.results[i].canonical_id;
 
+                //limit the number of recipes to 6, to match number of cards available 
                 for (k = 0; k < 6; k++) {
+                    //filter out recipe compilations 
                     if (responseId.includes("recipe")) {
                         recipeArray.push(response.results[i]);
 
@@ -40,25 +43,27 @@ $(document).ready(function () {
 
 
             };
-
+            //pull useful elements out of the array for display on cards
             for (l = 0; l < recipeArray.length; l++) {
                 var recipeTile = recipeArray[l].name;
                 var sections = recipeArray[l].sections
 
                 var componentsList = sections && sections[0].components;
                 var instructionList = recipeArray[l].instructions;
-
+                var thumbNail = recipeArray[l].thumbnail_url;
                 var newDescription = recipeArray[l].description;
                 console.log("Title: " + recipeTile);
                 console.log("Description: " + newDescription);
+                console.log("Img: "+thumbNail);
 
+                //parse through the ingredients array to create strings
                 if (componentsList) {
                     for (var j = 0; j < componentsList.length; j++) {
                         newItemText = componentsList[j].raw_text;
                         console.log(newItemText);
                     };
                 }
-
+                //parse through the instructions array to create strings 
                 if(instructionList){
                     for (var m = 0; m < instructionList.length; m++ ){
                         newInstruction = instructionList[m].display_text;
@@ -69,35 +74,7 @@ $(document).ready(function () {
             };
 
             console.log(recipeArray);
-            // var recipeTile = response.results[0].name;
-            // var displayRecipe = $("#itemDisplay").prepend("<h4>" + recipeTile);
-
-            // console.log(recipeTile);
-            // //where ingredients are stored 
-
-            // var ingredientList = $("#ingredientList");
-            // var newItem = $("<ls>");
-            // //raw string of ingredients
-            // var newItemText = componentsList.raw_text;
-            // console.log(response.results[0].sections[0].components)
-
-            // for (var i = 0; i < componentsList.length; i++) {
-            //     //parse the list of items
-            //     newItemText = componentsList[i].raw_text;
-            //     console.log(newItemText);
-            //     //add new html element 
-            //     // ingredientList.append(newItem).append("<li>" + newItemText);
-            // };
-
-            // var instructions = response.results[0].instructions[0].display_text;
-            // console.log(instructions);
-            // var instructionList = $("#instructionList");
-
-            // for (i = 0; instructions.length; i++) {
-            //     instuction = response.results[i].instructions[i].display_text;
-            //     console.log(instructions);
-            //     instructionList.append(newItem).append("<li>" + instructions);
-            // };
+            
         });
     });
 
