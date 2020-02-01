aaec724d9d5aa107c700a73c83e7b4ab6f0e57bd
cristian634 <cristian634@comcast.net>
2020-02-01 13:05:03 -0700

    cards display current array info

--- script/script.js
+++ script/script.js
@@ -1,9 +1,15 @@
 selectorEl = $("#selector");
+// let totalArray = // some call for default view || [];
+var rowClass = document.getElementById("cardsDisplay");
 
 
+
+
+
+// -------------------------------- AJAX CALL FOR API -------------------------------
 $(document).ready(function () {
     selectorEl.on("change", function () {
-        //search paramater pull from selector bar 
+
         var q = $(this).val();
         console.log(q);
         var settings = {
@@ -23,62 +29,143 @@ $(document).ready(function () {
 
             console.log(response);
 
-
+           
             for (i = 0; i < response.results.length; i++) {
-                //blank array for new recipes
                 recipeArray = [];
                 responseId = response.results[i].canonical_id;
 
-                //limit the number of recipes to 6, to match number of cards available 
                 for (k = 0; k < 6; k++) {
-                    //filter out recipe compilations 
                     if (responseId.includes("recipe")) {
-                        recipeArray.push(response.results[i]);
+                        recipeArray.push(response.results[k]);
+
 
                     }
                 }
 
-
-
-
-
             };
-            //pull useful elements out of the array for display on cards
+
             for (l = 0; l < recipeArray.length; l++) {
-                var recipeTile = recipeArray[l].name;
+                var recipeTitle = recipeArray[l].name;
                 var sections = recipeArray[l].sections
 
                 var componentsList = sections && sections[0].components;
                 var instructionList = recipeArray[l].instructions;
-                var thumbNail = recipeArray[l].thumbnail_url;
+                var newThumb = recipeArray[l].thumbnail_url;
                 var newDescription = recipeArray[l].description;
-                console.log("Title: " + recipeTile);
+                console.log("Title: " + recipeTitle);
                 console.log("Description: " + newDescription);
-                console.log("Img: "+thumbNail);
+                console.log("Thumbnail: " + newThumb);
 
-                //parse through the ingredients array to create strings
+                // ------------------ create elements with array info --------------------------
+
+
+                var newCard = document.createElement("div");
+                newCard.setAttribute("class", "uk-card uk-card-default uk-border-rounded");
+                var div2 = document.createElement("div");
+                var imgThumb = document.createElement("img");
+                imgThumb.setAttribute("class", "uk-transition-scale-up uk-transition-opaque");
+                imgThumb.setAttribute("src", newThumb);
+                // console.log(imgThumb);
+                var div3 = document.createElement("div");
+                div3.setAttribute("class", "uk-padding-small");
+                var titleSet = document.createElement("h3");
+                titleSet.setAttribute("class", "uk-card-title", recipeTitle);
+                titleSet = recipeTitle;
+                var descriptSet = document.createElement("p");
+                descriptSet = newDescription;
+
+
+
+                div3.append(titleSet, descriptSet);
+                div2.append(imgThumb)
+                newCard.append(div2, div3);
+                rowClass.append(newCard);
+
+
                 if (componentsList) {
                     for (var j = 0; j < componentsList.length; j++) {
                         newItemText = componentsList[j].raw_text;
                         console.log(newItemText);
                     };
                 }
-                //parse through the instructions array to create strings 
-                if(instructionList){
-                    for (var m = 0; m < instructionList.length; m++ ){
+
+                if (instructionList) {
+                    for (var m = 0; m < instructionList.length; m++) {
                         newInstruction = instructionList[m].display_text;
                         console.log(newInstruction);
                     };
                 };
-                console.log("--------------new recipe----------------")
+                console.log("--------------new recipe----------------");
             };
 
+
+
+            // for (i = 0; i < recipeArray.length; i++) {
+
+                // console.log(recipeTitle);
+                // console.log(newDescription);
+                // console.log($(".uk-card-title"));
+
+                // var newCard = document.createElement("div");
+                // newCard.setAttribute("class", "uk-card uk-card-default uk-border-rounded");
+                // var div2 = document.createElement("div");
+                // var imgThumb = document.createElement("img");
+                // imgThumb.setAttribute("class", "uk-transition-scale-up uk-transition-opaque");
+                // imgThumb.setAttribute("src", newImage);
+                // // console.log(imgThumb);
+                // var div3 = document.createElement("div");
+                // div3.setAttribute("class", "uk-padding-small");
+                // var titleSet = document.createElement("h3");
+                // titleSet.setAttribute("class", "uk-card-title", recipeTitle);
+                // titleSet = recipeTitle;
+                // var descriptSet = document.createElement("p");
+                // descriptSet = newDescription;
+
+
+
+                // div3.append(titleSet, descriptSet);
+                // div2.append(imgThumb)
+                // newCard.append(div2, div3);
+                // rowClass.append(newCard);
+                // console.log(rowClass);
+
+
+            // };
+            console.log(recipeArray.length)
             console.log(recipeArray);
-            
+
+
         });
-    });
+
+
+        //---------------------------QUOTE OF THE DAY---------------------------
+        var settingsQuote = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://andruxnet-random-famous-quotes.p.rapidapi.com/?count=1",
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "andruxnet-random-famous-quotes.p.rapidapi.com",
+                "x-rapidapi-key": "07a1619e66msh10bf90c8c184833p1c1590jsn24047b7c532a"
+            }
+        }
+
+        $.ajax(settingsQuote).done(function (response) {
+            console.log(response);
+            $("#quote").text('"' + response[0].quote + '" - ' + response[0].author);
+        });
 
 
 
 
-})
+
+
+    })
+    //---------------------------CARD CLICK------------------------------
+    $(".uk-card").on("click", function () {
+        var cardID = $(this).text();
+        console.log("register card click" + cardID);
+        callAPI(cardID);
+    })
+
+});
